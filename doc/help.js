0aadac2f17705d7f9668d056a480733eed3e87cd
Eliah Kagan <degeneracypressure@gmail.com>
2021-02-13 21:09:24 -0500

    better glyph and dynamic title text for h1 navlink

--- doc/help.js
+++ doc/help.js
@@ -3,7 +3,9 @@
 
     const params = new URLSearchParams(window.location.search);
     const idToMark = params.get('highlight');
-    if (idToMark !== null) {
-        document.getElementById(idToMark).classList.add('marked');
-    }
+    if (idToMark === null) return;
+
+    document.getElementById(idToMark).classList.add('marked');
+    const pagelink = document.getElementById('pagelink');
+    pagelink.title = 'Clear highlighting\u2009/\u2009' + pagelink.title;
 })();
